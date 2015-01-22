d4abad333736338c9bb2c71dffb8c9c835ce92ff
Miguel Angel Alarcos Torrecillas <miguel.alarcos@gmail.com>
2015-01-22 10:47:31 +0100

    release of version v0.1.5

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'miguelalarcos:xautocomplete',
   summary: 'An autocomplete widget. Values can be strings, array of strings or reference _ids.',
-  version: '0.1.4',
+  version: '0.1.5',
   git: 'https://github.com/miguelalarcos/xautocomplete.git'
 });
 
